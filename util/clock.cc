95db938113acd885cac78987eb326b4526d7cdce
James Kuszmaul <jabukuszmaul+collab@gmail.com>
2016-10-30 21:44:55 -0400

    Added some clock information

--- util/clock.cc
+++ util/clock.cc
@@ -7,6 +7,8 @@
 namespace sailbot {
 namespace util {
 
+bool monotonic_clock::fake_clock = false;
+
 namespace {
   bool time_is_greater(timespec rhs, timespec lhs) {
     return rhs.tv_sec == lhs.tv_sec ? rhs.tv_nsec > lhs.tv_nsec
@@ -35,6 +37,56 @@ void Loop::WaitForNext() {
   } while (time_is_greater(time, last_trigger_));
   clock_nanosleep(clock_, TIMER_ABSTIME /*flags*/, &last_trigger_,
                   NULL /*remain*/);
+}
+
+monotonic_clock::time_point monotonic_clock::now() {
+  if (!fake_clock) {
+    timespec time;
+    clock_gettime(clock_, &time);
+    return time_point(duration(time.tv_nsec + time.tv_sec * 1000000000LL));
+  } else {
+    return time_point(duration(time_));
+  }
+}
+
+void monotonic_clock::sleep_until(time_point time,
+                                  std::shared_lock<std::shared_timed_mutex> &l) {
+  rep len = time.time_since_epoch().count();
+  if (!fake_clock) {
+    timespec ts;
+    ts.tv_sec = len / 1000000000LL;
+    ts.tv_nsec = len - ts.tv_sec * 1000000000LL;
+    clock_nanosleep(clock_, TIMER_ABSTIME /*flags*/, &ts, NULL);
+  } else {
+    {
+      std::unique_lock<std::mutex> lck(wakeup_time_mutex_);
+      if (next_wakeup_ < time_) {
+        next_wakeup_ = std::max(len, time_.load());
+      } else {
+        next_wakeup_ = std::min(next_wakeup_, len);
+      }
+    }
+    tick_.wait(l, [&]() { return len <= time_; });
+  }
+}
+
+ClockInstance::ClockInstance() : lck_(m_) {}
+
+monotonic_clock::time_point ClockInstance::Time() {
+  return monotonic_clock::now();
+}
+
+void ClockInstance::SleepUntil(monotonic_clock::time_point time) {
+  monotonic_clock::sleep_until(time, lck_);
+}
+
+void ClockManager::Run() {
+  if (monotonic_clock::is_fake()) {
+    while (true) {
+      std::unique_lock<std::shared_timed_mutex> lck(ClockInstance::m_);
+      monotonic_clock::set_time(monotonic_clock::next_wakeup_);
+    }
+  }
 }
 
 }  // util
--- util/clock.h
+++ util/clock.h
@@ -2,6 +2,13 @@
 #pragma once
 
 #include <time.h>
+#include <chrono>
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+#include <shared_mutex>
+#include <atomic>
+#include <set>
 
 namespace sailbot {
 namespace util {
@@ -20,6 +27,58 @@ class Loop {
   clockid_t clock_;
   timespec period_;
   timespec last_trigger_;
+};
+
+class monotonic_clock {
+ public:
+  typedef std::chrono::nanoseconds duration;
+  typedef duration::rep rep;
+  typedef duration::period period;
+  typedef std::chrono::time_point<monotonic_clock> time_point;
+
+  static constexpr clockid_t clock_ = CLOCK_MONOTONIC;
+  static constexpr bool is_steady = true;
+
+  static time_point now();
+
+  static void sleep_until(time_point time,
+                          std::shared_lock<std::shared_timed_mutex>& l);
+
+  static bool is_fake() { return fake_clock; }
+
+ private:
+  static bool fake_clock;
+  static std::atomic<rep> time_;
+  static std::condition_variable_any tick_;
+  static rep next_wakeup_;
+  static std::mutex wakeup_time_mutex_;
+
+  static void set_time(rep time) {
+    time_ = time;
+    tick_.notify_all();
+  }
+
+  friend class ClockManager;
+};
+
+class ClockInstance {
+ public:
+  ClockInstance();
+  inline monotonic_clock::time_point Time();
+  void SleepUntil(monotonic_clock::time_point time);
+ private:
+  static std::shared_timed_mutex m_;
+  std::shared_lock<std::shared_timed_mutex> lck_;
+
+  friend class ClockManager;
+};
+
+class ClockManager {
+ public:
+  static void SetFakeClock(bool is_fake) {
+    monotonic_clock::fake_clock = is_fake;
+  }
+  static void Run();
 };
 
 }  // util
