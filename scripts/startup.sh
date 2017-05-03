73392cd72d2e842b864fc41ebe2c442ec141cfdf
James Kuszmaul <jabukuszmaul+collab@gmail.com>
2017-05-02 20:24:56 -0400

    Minor UI/script updates

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -7,7 +7,7 @@ echo "/home/debian/cores/core.%p" > /proc/sys/kernel/core_pattern
 DIR="$( cd "$( dirname $0 )" && pwd )"
 $DIR/bringup-can.sh
 sleep 1 # Shouldn't be necessary, but give can interface time to come up
-$DIR/logger_main -logfilename /home/debian/logfile-$(date +%s) &
+$DIR/logger_main -logfilename /home/debian/logs/logfile-$(date +%s) &
 $DIR/can-dump &
 $DIR/server_main &
 $DIR/simple_control_main &
