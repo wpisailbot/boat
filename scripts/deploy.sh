87f6f65bfa3ec6e4c234bdd5bb2f0a6c75f49986
James Kuszmaul <jabukuszmaul+collab@gmail.com>
2017-11-03 19:35:47 -0400

    Add parts to script to remotely reboot code

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,5 +10,14 @@ scp $1 $TARGET:/tmp/output.zip
 scp $2 $TARGET:/tmp/site.zip
 ssh $TARGET "unzip -o /tmp/output.zip -d ~/bin/sailbot"
 ssh $TARGET "unzip -o /tmp/site.zip -d ~/bin/sailbot/html"
+
+# Restart all the code if another argument is passed
+if [ -n "$4" ]
+  then
+  ssh $TARGET "sudo /home/debian/bin/sailbot/killall.sh"
+  sleep 2
+  ssh $TARGET "sudo /home/debian/bin/sailbot/startup.sh 2>/dev/null >/dev/null"
+fi
+
 # The argument to sync may not actually do anything...
 ssh $TARGET "sync ~/bin/sailbot/*"
