01965eb723a600d78d2449951139bca2b747b35a
James Kuszmaul <jabukuszmaul+collab@gmail.com>
2018-01-18 10:11:30 -0500

    Add movable ballast matlab files

---
+++ control/matlab/calcTransform.m
@@ -0,0 +1,8 @@
+function dh = calcTransform(d, theta, a, alpha)
+
+dh = [cos(theta), -sin(theta)*cos(alpha), sin(theta)*sin(alpha),  a*cos(theta);
+      sin(theta), cos(theta)*cos(alpha),   -cos(theta)*sin(alpha), a*sin(theta);
+      0,          sin(alpha),             cos(alpha),             d;
+      0,          0,                      0,                      1];
+
+end
