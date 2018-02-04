7f067cb511baa53c9efc564d9a0e2e8cc9ec7a04
James Kuszmaul <jabukuszmaul+collab@gmail.com>
2018-02-04 16:51:04 -0500

    Playing around with movable ballast

--- control/matlab/calcMBRightingMoment.m
+++ control/matlab/calcMBRightingMoment.m
@@ -2,7 +2,7 @@ function [ rightingMoment ] = calcMBRightingMoment( heelAngle, deviationFromCent
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 MBShaftElevation = deg2rad(28.6); % deg
-verticalShaftOffset = 7.21; % in
+verticalShaftOffset = 7.21 * .0254; % m
 
 tf_BoatCenterline_Heeled = calcTransform(0, heelAngle,0, pi/2);
 tf_Heeled_ShaftBase = calcTransform(verticalShaftOffset, 0, 0, MBShaftElevation);
@@ -10,7 +10,7 @@ tf_ShaftBase_ArmCS = calcTransform(0, deviationFromCenter, 0, 0);
 
 tf_WCS_ArmCS = tf_BoatCenterline_Heeled*tf_Heeled_ShaftBase*tf_ShaftBase_ArmCS;
 
-armInWCS = tf_WCS_ArmCS*[0; 25.25; -7.9; 1];
+armInWCS = tf_WCS_ArmCS*[0; 25.25; -7.9; 1] * .0254;
 
 rightingMoment = armInWCS(1)*load;
 
