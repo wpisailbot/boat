481d2cb6ab60d505341222e07f1d4d9efcc05986
James Kuszmaul <jabukuszmaul+collab@gmail.com>
2018-02-12 16:17:03 -0500

    Make potential controller, has saturation isssues

--- control/matlab/calcTransform.m
+++ control/matlab/calcTransform.m
@@ -1,8 +1,13 @@
-function dh = calcTransform(d, theta, a, alpha)
+function [dh, dhdtheta] = calcTransform(d, theta, a, alpha)
 
 dh = [cos(theta), -sin(theta)*cos(alpha), sin(theta)*sin(alpha),  a*cos(theta);
       sin(theta), cos(theta)*cos(alpha),   -cos(theta)*sin(alpha), a*sin(theta);
       0,          sin(alpha),             cos(alpha),             d;
       0,          0,                      0,                      1];
+
+dhdtheta = [-sin(theta), -cos(theta)*cos(alpha), cos(theta)*sin(alpha),  -a*sin(theta);
+            cos(theta), -sin(theta)*cos(alpha),   sin(theta)*sin(alpha), a*cos(theta);
+            0,          0,             0,             0;
+            0,          0,                      0,                      0];
 
 end
