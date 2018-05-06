c6681d885ae5af59aae29bc01400e3ce972b0c0e
James Kuszmaul <jabukuszmaul+collab@gmail.com>
2018-05-05 21:15:06 -0400

    Reduce starvation in log replay

--- ipc/test_queue.cc
+++ ipc/test_queue.cc
@@ -19,10 +19,21 @@ void TestQueue::send(const void *msg, size_t size) {
   memcpy(data.data.get(), msg, std::min(size, max_size_));
   data.data_len = std::min(size, max_size_);
   data.cond.notify_all();
+  data.cond.wait_for(lck_, std::chrono::milliseconds(50), [this, &data]() {
+    if (util::IsShutdown())
+      return true;
+    for (const int n : data.users) {
+      if (n >= 0 && n < data.inc) {
+        return false;
+      }
+    }
+    return true;
+  });
 }
 
 bool TestQueue::receive(void *msg, size_t size, size_t &rcvd) {
   std::unique_lock<std::mutex> lck_(map_lock_);
+  if (last_inc_ == 0) conditions_[name_].users[idx_] = 0;
   // Construct in place if needed.
   if (conditions_[name_].inc == last_inc_) {
     // Wait for the data...
@@ -30,6 +41,8 @@ bool TestQueue::receive(void *msg, size_t size, size_t &rcvd) {
       conditions_[name_].cond.wait_for(lck_, std::chrono::milliseconds(50));
     }
   }
+  conditions_[name_].users[idx_] = conditions_[name_].inc;
+  conditions_[name_].cond.notify_all();
   if (conditions_[name_].data) {
     rcvd = std::min(size, conditions_[name_].data_len);
     memcpy(msg, conditions_[name_].data.get(), rcvd);
--- ipc/test_queue.h
+++ ipc/test_queue.h
@@ -3,6 +3,7 @@
 #include <condition_variable>
 #include <shared_mutex>
 #include <mutex>
+#include <vector>
 #include <atomic>
 #include <string>
 
@@ -15,10 +16,13 @@ class TestQueue {
       : name_(name), max_size_(max_size) {
     std::unique_lock<std::mutex> lck_(map_lock_);
     conditions_[name_].cnt++;
+    idx_ = conditions_[name_].users.size();
+    conditions_[name_].users.push_back(-1);
   }
   ~TestQueue() {
     std::unique_lock<std::mutex> lck_(map_lock_);
     conditions_[name_].cnt--;
+    conditions_[name_].users[idx_] = -1;
     if (conditions_[name_].cnt.load() == 0) {
       conditions_.erase(name_);
     }
@@ -30,6 +34,7 @@ class TestQueue {
     std::condition_variable cond;
     std::unique_ptr<uint8_t[]> data;
     std::atomic<int> cnt{0};
+    std::vector<int> users;
     size_t data_len = 0;
     int inc = 0;
   };
@@ -40,6 +45,7 @@ class TestQueue {
   const size_t max_size_;
   // Assumes that last_inc_ is called in only one thread.
   int last_inc_ = 0;
+  int idx_ = -1; // Index in users vector
 };
 
 }  // testing
