bcb00a993572252c19e3fa15fcc4365c4392d589
sfisher25 <syd.fisher@gmail.com>
2019-01-14 16:35:29 -0500

    Update and rename enduranceTest.c to enduranceTest.cc

--- Sailbot18_19Strategy/Endurance Challenge/enduranceTest.cc
+++ Sailbot18_19Strategy/Endurance Challenge/enduranceTest.cc
@@ -1,40 +1,5 @@
-/*Endurance/Long Distance event
-*
-* As taken from the sailbot 2018 events website:
-* Challenge goal: To demonstarate the boat's durability and capabitlity to sail 
-* some distance
-*
-* Description: The boats will sail around 4 buoys (passing within 10 m inside of 
-* a buoy is OK) for up to 7 hours
-*
-* This code was written by Sydney Fisher and Sierra Palmer as part of the WPI 
-* 2018-2019 Sailbot team to accomplish this task
-*/
 
-double time; // elapsed time in seconds
-double endRaceTime = 25200; // number of seconds in 7 hours
-double R; // the radius from the buoy centroid to the outer point of the buoy
-float waypoints[2][3]; 
-float obstacleGate[4][3];
-
-int main(){
-	// Set the clock to 0 and have it start running for the duration of the 
-	// event
-	clockTimer();
-	
-	// continue sailing until the end of the race
-	while(time < endRaceTime){
-		// keep having the boat sail forward unitl there is a buoy detected, then 
-		// keep moving through the code
-		while(buoyDetect()!= TRUE){
-			buoyDetect();
-		}
-		findCentroid();
-		setObstacleGate();
-		sailTo();
-		resetPoints();
-	}
-}
+#include "enduranceTest.h"
 
 void clockTimer(){
 	// Use some built in clock, set it to 0 at the start, and then check 
