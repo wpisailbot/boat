8cee67d5bb9bf443bb3254d58265dad2510a619f
sfisher25 <syd.fisher@gmail.com>
2019-01-14 12:24:54 -0500

    Added challenge definition to stationKeeping_main.cc

--- Sailbot18_19Strategy/stationKeeping_main.cc
+++ Sailbot18_19Strategy/stationKeeping_main.cc
@@ -1,3 +1,16 @@
+/* Station Keeping
+*
+* As taken from the sailbot 2018 events website:
+* Challenge goal: To demonstrate the ability of the boat to remain close to one position
+* and respond to time-based commands.
+*
+* Description: The boat will enter a 40x40 m box adn attempt to stay inside the box for 
+* 5 minutes. It must then exit within 30 seconds to avoid a penalty.
+*
+* This code was written by Sydney Fisher and Sierra Palmer as part of the WPI 
+* 2018-2019 Sailbot team to accomplish this task
+*/
+
 
 #include "stationKeeping.h"
 
