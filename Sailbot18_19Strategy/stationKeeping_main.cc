3e7c81a8407beb31b7eea5351c43b9e567febd52
sfisher25 <syd.fisher@gmail.com>
2019-01-14 12:17:25 -0500

    Added the int main()

--- Sailbot18_19Strategy/stationKeeping_main.cc
+++ Sailbot18_19Strategy/stationKeeping_main.cc
@@ -1,1 +1,23 @@
 
+#include "stationKeeping.h"
+
+int main(){
+	// Set the clock to 0 and have it start running for the duration of the 
+	// event
+	clockTimer();
+	sailThroughGate(); // sail into the box
+
+	while(timer < keepingTime){ // Has the boat count down the time to stay inside box
+		resetPoints();
+		centerTackAndLocate();
+		createObstacleFence();
+		boxSailing();
+	}
+
+	time = 0;
+	resetPoints();
+
+	while(timer < exitTime){
+		sailThroughGate();
+	}
+}
