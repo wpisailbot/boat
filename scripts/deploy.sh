ac345302153f5d96d01612f33737b4c69585eb8b
Sierra Palmer <stpalmer@wpi.edu>
2019-01-31 19:21:56 -0500

    updated fixes to run rudders

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ set -e
 ssh $TARGET "rm -f /tmp/output.zip"
 ssh $TARGET "rm -f /tmp/site.zip"
 scp $1 $TARGET:/tmp/output.zip
-scp $2 $TARGET:/tmp/site.zipter
+scp $2 $TARGET:/tmp/site.zip
 ssh $TARGET "unzip -o /tmp/output.zip -d ~/bin/sailbot"
 ssh $TARGET "unzip -o /tmp/site.zip -d ~/bin/sailbot/html"
 
