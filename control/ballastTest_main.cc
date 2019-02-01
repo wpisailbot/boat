d3243725cec57fff93cde8b6cfe216ede3cb9bd2
sfisher25 <syd.fisher@gmail.com>
2019-02-01 10:42:03 -0500

    Update ballastTest_main.cc

--- control/ballastTest_main.cc
+++ control/ballastTest_main.cc
@@ -1,1 +1,13 @@
-B
+//
+// Created by Sydney Fisher on 2/1/19.
+//
+
+#include "simple.h"
+#include "ballastTest.h"
+
+int main(int argc, char *argv[]){
+    sailbot::util::SetCurrentThreadRealtimePriority(10);
+    sailbot::util::Init(argc, argv);
+
+    sailbot::control::BallastTest::simpleBallast();
+}
