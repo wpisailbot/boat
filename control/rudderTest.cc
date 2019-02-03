6067d3a7aabacf9183a1a1e57c82ed04a5be924a
Sierra Palmer <stpalmer@wpi.edu>
2019-02-03 10:46:31 -0500

    trying to figure out how to correctly call msgs

--- control/rudderTest.cc
+++ control/rudderTest.cc
@@ -15,8 +15,8 @@ namespace control{
 
     void RudderTest::simpleRudder(){
             if (rudder){
-                control::SimpleControl::msg::RudderCmd::rudder_msg_->set_pos(0.75);
-                control::SimpleControl::msg::RudderCmd::rudder_cmd_.send(rudder_msg_);
+                control::SimpleControl::msg::rudder_msg_->set_pos(0.75);
+                control::SimpleControl::msg::rudder_cmd_.send(rudder_msg_);
             }
             else {
                 msg::RudderCmd::rudder_msg_->set_pos(0);
