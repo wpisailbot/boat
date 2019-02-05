cdff4c086eebebc395162ad33d7ade78fb0fd19b
Sierra Palmer <stpalmer@wpi.edu>
2019-02-05 16:37:57 -0500

    anyone

--- control/rudderTest_main.cc
+++ control/rudderTest_main.cc
@@ -4,15 +4,15 @@
 
 #include "simple.h"
 #include "rudderTest.h"
-package RudderTest;
+//package RudderTest;
 
 int main(int argc, char *argv[]){
-    RudderTest rt;
+    //RudderTest rt;
     const bool rudderTest = true;
 
     sailbot::util::SetCurrentThreadRealtimePriority(10);
     sailbot::util::Init(argc, argv);
 
-    rt.simpleRudder(rudderTest);
+    sailbot::control::RudderTest::simpleRudder(rudderTest);
 }
 
