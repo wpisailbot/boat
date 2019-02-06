7bb4884e6c3143d728ed820c893d3c9c1fa095df
sfisher25 <syd.fisher@gmail.com>
2019-02-05 19:58:45 -0500

    Update rudderTest.cc

--- control/rudderTest.cc
+++ control/rudderTest.cc
@@ -11,22 +11,34 @@
 #include "control/util.h"
 
 namespace sailbot {
-namespace control{
+namespace control {
 
-    void RudderTest::simpleRudder(bool rudder = true, rudder_msg_(AllocateMessage<msg::RudderCmd>()), rudder_cmd_("rudder_cmd", true)){
+     RudderTest::SimpleControlTest(bool do_rudderTest)
+        : Node(0.01),
+          do_rudderTest_(do_rudderTest),
+          rudder_msg_(AllocateMessage<msg::RudderCmd>()),
+          rudder_cmd_("rudder_cmd", true) {
+          
+     consts_msg_->set_max_rudder(0.5);
+     consts_msg_->set_rudder_kp(0.5);
+     consts_msg_->set_rudder_ki(0.02);
+     
+     }
+    
+    void RudderTest::simpleRudder(bool rudder = true, rudder_msg_(AllocateMessage<msg::RudderCmd()), rudder_cmd_("rudder_cmd", true)){
         if (RudderTest::rudderNew){
 
-            //sailbot::msg::RudderCmd
+            sailbot::msg::RudderCmd
 
             rudder_msg_->set_pos(0.75); // error with needing an initializer
-            //sailbot::ProtoQueue<msg::RudderCmd>
+            sailbot::ProtoQueue<msg::RudderCmd>
             rudder_cmd_.send(rudder_msg_);
         }
         else {
-            //sailbot::msg::RudderCmd
+            sailbot::msg::RudderCmd
             rudder_msg_->set_pos(0);
         }
-    }
+    } */
 } //control
 } //sailbot
 
