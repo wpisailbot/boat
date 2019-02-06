5809d6ecb636f1d2d10d07c1d59022154442cecf
Sierra Palmer <stpalmer@wpi.edu>
2019-02-06 16:30:33 -0500

    fixing more syntax errors 2.7

--- control/rudderTest_main.cc
+++ control/rudderTest_main.cc
@@ -7,7 +7,7 @@
 //package RudderTest;
 
 int main(int argc, char *argv[]){
-    sailbot::control::RudderTest rt;
+    const sailbot::control::RudderTest rt;
     sailbot::util::SetCurrentThreadRealtimePriority(10);
     sailbot::util::Init(argc, argv);
     
