94449198cbd66e638c33911e71deb4e6f9fe540a
sfisher25 <syd.fisher@gmail.com>
2019-02-09 16:00:26 -0500

    Update ServoTestCode.py

--- ServoTestCode.py
+++ ServoTestCode.py
@@ -1,18 +1,20 @@
 import Adafruit_BBIO.PWM as PWM
 
 servo_pin = "P8_13"
-duty_min = 3
+duty_min = 50
 duty_max = 14.5
 duty_span = duty_max - duty_min
 
-PWM.start(servo_pin, (100-duty_min), 60.0)
+PWM.start(servo_pin, (100-duty_min), 1000.0)
 
-while True:
+boolean run = true;
+while run:
     angle = raw_input("Angle (0 to 180 x to exit):")
     if angle == 'x':
         PWM.stop(servo_pin)
         PWM.cleanup()
-        break
+        run=false;
+        break;
     angle_f = float(angle)
     duty = 100 - ((angle_f / 180) * duty_span + duty_min) 
     PWM.set_duty_cycle(servo_pin, duty)
