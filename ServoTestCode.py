1c51cf45f182a6ca4e379c78d80b98b9a74d6914
sfisher25 <syd.fisher@gmail.com>
2019-02-10 10:21:50 -0500

    Update ServoTestCode.py

--- ServoTestCode.py
+++ ServoTestCode.py
@@ -7,13 +7,11 @@ duty_span = duty_max - duty_min
 
 PWM.start(servo_pin, (100-duty_min), 1000.0)
 
-boolean run = true;
 while run:
     angle = raw_input("Angle (0 to 180 x to exit):")
     if angle == 'x':
         PWM.stop(servo_pin)
         PWM.cleanup()
-        run=false;
         break;
     angle_f = float(angle)
     duty = 100 - ((angle_f / 180) * duty_span + duty_min) 
