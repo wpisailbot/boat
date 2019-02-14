b7a7cabfb49cb40038ac789441d1dff6769593a8
sfisher25 <syd.fisher@gmail.com>
2019-02-14 13:09:18 -0500

    Update rudderTest.cc

--- control/rudderTest.cc
+++ control/rudderTest.cc
@@ -1,3 +1,4 @@
+
 //
 // Created by Sierra Palmer and Sydney Fisher on 1/28/19. 
 //
@@ -9,6 +10,7 @@
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 #include "control/util.h"
+#include <iostream>
 
 namespace sailbot {
 namespace control {
@@ -21,29 +23,21 @@ namespace control {
 	  consts_msg_(AllocateMessage<msg::ControllerConstants>())
                                                                    {
           
-     consts_msg_->set_max_rudder(0.5);
-     consts_msg_->set_rudder_kp(0.5);
-     consts_msg_->set_rudder_ki(0.02);
+     //consts_msg_->set_max_rudder(0.5);
+     //consts_msg_->set_rudder_kp(0.5);
+     //consts_msg_->set_rudder_ki(0.02);
      
 }
     
-    void RudderTest::simpleRudder(bool rudder){
-    //,  rudder_msg_(AllocateMessage<msg::RudderCmd>()), rudder_cmd_("rudder_cmd", true)
-    while(true){
-        if (RudderTest::rudderNew){
-
-
-            rudder_msg_->set_pos(0.75); // error with needing an initializer
-            //sailbot::ProtoQueue<msg::RudderCmd>
-            rudder_cmd_.send(rudder_msg_);
-	    LOG(INFO) << "Sending rudder message: " << rudder_cmd_->ShortDebugString();
-        }
-        else {
-            //sailbot::msg::RudderCmd
-            rudder_msg_->set_pos(.5);
-	    rudder_cmd_.send(rudder_msg_);
-        }
+  void RudderTest::Iterate() {
+    counter += 0.001;
+    if (counter > 1.0) {
+      counter = 0.0;
     }
-    } 
+    rudder_msg_->set_pos(counter);
+    rudder_cmd_.send(rudder_msg_);
+    LOG(INFO) << "Send rudder command:" << rudder_msg_->ShortDebugString();
+  }
+
 } //control
 } //sailbot
